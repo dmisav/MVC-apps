605892f43233dec61c398055fc2fb40110088b59
dmisav <dmitry.savchenko@miratechgroup.com>
2014-10-17 14:47:48 +0300

    refactored item displaying markup and javascript

--- FrontPagePackages/FrontPagePackages/Scripts/Controllers/PackageController.js
+++ FrontPagePackages/FrontPagePackages/Scripts/Controllers/PackageController.js
@@ -1,4 +1,4 @@
-﻿var app = angular.module('PackagesModule',[]);
+﻿var app = angular.module('PackagesModule', []);
 
 app.controller('PackageController', function ($http) {
     var PkgCtrl = this;
@@ -12,18 +12,22 @@ app.controller('PackageController', function ($http) {
         GetAllPackages();
     }
 
-    PkgCtrl.SetSelectedPkg = function(packageParam) {
+    PkgCtrl.GetItemClass = function (item) {
+        var index = PkgCtrl.items.indexOf(item);
+        var enabled = IsItemEnabled(item.DisplayName);
+        return "item" + index + " " + enabled;
+    }
+
+    PkgCtrl.SetSelectedPkg = function (packageParam) {
         PkgCtrl.selectedPkg = packageParam;
     }
 
-    PkgCtrl.IsCurrent = function(packageParam)
-    {
+    PkgCtrl.IsCurrent = function (packageParam) {
         return packageParam == PkgCtrl.selectedPkg;
     }
 
-    PkgCtrl.IsItemEnabled = function (itemName) {
-        if (!isObjectEmpty(PkgCtrl.selectedPkg))
-        {
+    function IsItemEnabled(itemName) {
+        if (!isObjectEmpty(PkgCtrl.selectedPkg)) {
             for (var i = PkgCtrl.selectedPkg.items.length - 1; i > -1; i--) {
                 if (PkgCtrl.selectedPkg.items[i].DisplayName === itemName)
                     return "circleEnabled";
